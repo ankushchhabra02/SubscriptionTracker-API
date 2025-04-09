566895393d55ce2b3627dc8247cc0c978a4571ff
Ankush Chhabra <ankushchhabra2002@gmail.com>
2025-04-09 11:51:50 +0530

    subscription controllers

---
+++ controllers/subscription.controller.js
@@ -0,0 +1,133 @@
+import Subscription from "../models/subscription.model.js";
+
+// CREATE (Already implemented)
+export const createSubscription = async (req, res, next) => {
+  try {
+    const subscription = await Subscription.create({
+      ...req.body,
+      user: req.user._id,
+    });
+
+    res.status(201).json({ success: true, data: subscription });
+  } catch (e) {
+    next(e);
+  }
+};
+
+// GET User Subscriptions (Already implemented)
+export const getUserSubscriptions = async (req, res, next) => {
+  try {
+    if (req.user.id !== req.params.id) {
+      const error = new Error("You are not the owner of this account");
+      error.status = 401;
+      throw error;
+    }
+
+    const subscriptions = await Subscription.find({ user: req.params.id });
+
+    res.status(200).json({ success: true, data: subscriptions });
+  } catch (e) {
+    next(e);
+  }
+};
+
+// GET all subscriptions (for admin or internal purposes)
+export const getAllSubscriptions = async (req, res, next) => {
+  try {
+    const subscriptions = await Subscription.find();
+    res.status(200).json({ success: true, data: subscriptions });
+  } catch (e) {
+    next(e);
+  }
+};
+
+// GET single subscription details
+export const getSubscriptionById = async (req, res, next) => {
+  try {
+    const subscription = await Subscription.findById(req.params.id);
+    if (!subscription) {
+      const error = new Error("Subscription not found");
+      error.status = 404;
+      throw error;
+    }
+    res.status(200).json({ success: true, data: subscription });
+  } catch (e) {
+    next(e);
+  }
+};
+
+// UPDATE subscription
+export const updateSubscription = async (req, res, next) => {
+  try {
+    const subscription = await Subscription.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    );
+
+    if (!subscription) {
+      const error = new Error("Subscription not found");
+      error.status = 404;
+      throw error;
+    }
+
+    res.status(200).json({ success: true, data: subscription });
+  } catch (e) {
+    next(e);
+  }
+};
+
+// DELETE subscription
+export const deleteSubscription = async (req, res, next) => {
+  try {
+    const subscription = await Subscription.findByIdAndDelete(req.params.id);
+
+    if (!subscription) {
+      const error = new Error("Subscription not found");
+      error.status = 404;
+      throw error;
+    }
+
+    res.status(200).json({ success: true, message: "Subscription deleted" });
+  } catch (e) {
+    next(e);
+  }
+};
+
+// CANCEL subscription
+export const cancelSubscription = async (req, res, next) => {
+  try {
+    const subscription = await Subscription.findById(req.params.id);
+
+    if (!subscription) {
+      const error = new Error("Subscription not found");
+      error.status = 404;
+      throw error;
+    }
+
+    subscription.status = "cancelled";
+    await subscription.save();
+
+    res.status(200).json({ success: true, data: subscription });
+  } catch (e) {
+    next(e);
+  }
+};
+
+// GET upcoming renewals
+export const getUpcomingRenewals = async (req, res, next) => {
+  try {
+    const now = new Date();
+    const upcomingDate = new Date(now);
+    upcomingDate.setDate(now.getDate() + 7); // upcoming in next 7 days
+
+    const subscriptions = await Subscription.find({
+      renewalDate: { $gte: now, $lte: upcomingDate },
+      status: "active",
+    });
+
+    res.status(200).json({ success: true, data: subscriptions });
+  } catch (e) {
+    next(e);
+  }
+};
--- routes/subscription.routes.js
+++ routes/subscription.routes.js
@@ -1,37 +1,25 @@
 import { Router } from "express";
+import authorize from "../middlewares/auth.middleware.js";
+import {
+  createSubscription,
+  getUserSubscriptions,
+  getAllSubscriptions,
+  getSubscriptionById,
+  updateSubscription,
+  deleteSubscription,
+  cancelSubscription,
+  getUpcomingRenewals,
+} from "../controllers/subscription.controller.js";
 
 const subscriptionRouter = Router();
 
-subscriptionRouter.get("/", (req, res) =>
-  res.send({ title: "GET all subscriptions" })
-);
-
-subscriptionRouter.get("/:id", (req, res) =>
-  res.send({ title: "GET subscription details" })
-);
-
-subscriptionRouter.post("/", (req, res) =>
-  res.send({ title: "CREATE subscription" })
-);
-
-subscriptionRouter.put("/:id", (req, res) =>
-  res.send({ title: "UPDATE subscription" })
-);
-
-subscriptionRouter.delete("/:id", (req, res) =>
-  res.send({ title: "DELETE subscription" })
-);
-
-subscriptionRouter.get("/user/:id", (req, res) =>
-  res.send({ title: "GET all user subscriptions" })
-);
-
-subscriptionRouter.put("/:id/cancel", (req, res) =>
-  res.send({ title: "CANCEL subscription" })
-);
-
-subscriptionRouter.get("/upcoming-renewals", (req, res) =>
-  res.send({ title: "GET upcoming renewals" })
-);
+subscriptionRouter.get("/", getAllSubscriptions);
+subscriptionRouter.get("/:id", getSubscriptionById);
+subscriptionRouter.post("/", authorize, createSubscription);
+subscriptionRouter.put("/:id", updateSubscription);
+subscriptionRouter.delete("/:id", deleteSubscription);
+subscriptionRouter.get("/user/:id", authorize, getUserSubscriptions);
+subscriptionRouter.put("/:id/cancel", cancelSubscription);
+subscriptionRouter.get("/upcoming-renewals", getUpcomingRenewals);
 
 export default subscriptionRouter;
